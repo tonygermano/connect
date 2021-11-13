bc0c9af90a15bf9a4b9a63db3f1cc7f4d6092599
Tony Germano <tony@germano.name>
2021-11-13 05:09:48 -0500

    JSObjConverter call JSON.stringify in js context
    Starting with Rhino 1.7.14, NativeJSON.stringify will no longer be able
    to be called with a null context. By calling in javascript code, we can
    also avoid needing to have a custom NativeDate class.  Furthermore,
    changing canConvert to test if the type implements Scriptable removes
    the need for a custom NativeBoolean and also catches classes like
    NativeString and NativeNumber that were previously not accounted for.

--- server/src/com/mirth/connect/model/converters/JavaScriptObjectConverter.java
+++ server/src/com/mirth/connect/model/converters/JavaScriptObjectConverter.java
@@ -9,13 +9,16 @@
 
 package com.mirth.connect.model.converters;
 
-import org.mozilla.javascript.NativeArray;
-import org.mozilla.javascript.NativeBoolean;
-import org.mozilla.javascript.NativeDate;
-import org.mozilla.javascript.NativeJSON;
-import org.mozilla.javascript.NativeObject;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Script;
 import org.mozilla.javascript.Scriptable;
 
+import com.mirth.connect.server.controllers.ContextFactoryController;
+import com.mirth.connect.server.controllers.ControllerFactory;
+import com.mirth.connect.server.util.javascript.JavaScriptScopeUtil;
+import com.mirth.connect.server.util.javascript.MirthContextFactory;
 import com.thoughtworks.xstream.converters.MarshallingContext;
 import com.thoughtworks.xstream.converters.reflection.ReflectionConverter;
 import com.thoughtworks.xstream.core.JVM;
@@ -23,6 +26,16 @@ import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import com.thoughtworks.xstream.mapper.Mapper;
 
 public class JavaScriptObjectConverter extends ReflectionConverter {
+       
+    private static class DelayedInitializedValues {
+        private static final ContextFactoryController contextFactoryController = ControllerFactory.getFactory().createContextFactoryController();
+        private static final MirthContextFactory mirthContextFactory = contextFactoryController.getGlobalContextFactory();
+    }
+    
+    // We don't want the quotes around the date, so call toISOString directly
+    private static final String conversionScript = "value instanceof Date ? value.toISOString() : JSON.stringify(value);";
+    
+    private static final AtomicReference<Script> compiledConversionScript = new AtomicReference<>();
 
     public JavaScriptObjectConverter(Mapper mapper) {
         super(mapper, JVM.newReflectionProvider());
@@ -30,20 +43,31 @@ public class JavaScriptObjectConverter extends ReflectionConverter {
 
     @Override
     public boolean canConvert(Class type) {
-        return type == NativeObject.class || type == NativeArray.class || type == NativeDate.class || type == NativeBoolean.class;
+        return Scriptable.class.isAssignableFrom(type);
     }
 
     @Override
     public void marshal(Object value, HierarchicalStreamWriter writer, MarshallingContext context) {
         try {
-            if (value instanceof NativeDate) {
-                // We don't want the quotes around the date, so call toISOString directly
-                context.convertAnother(NativeDate.js_toISOString(((NativeDate) value).getJSTimeValue()));
-            } else {
-                context.convertAnother(NativeJSON.stringify(null, (Scriptable) value, value, null, null).toString());
-            }
+            final Scriptable scope = JavaScriptScopeUtil.getDeployScope(DelayedInitializedValues.mirthContextFactory, null);
+            final Context jsContext = Context.getCurrentContext();
+            final Script script = getCompiledScript(jsContext);
+            scope.put("value", scope, value);
+            context.convertAnother(script.exec(jsContext, scope));
         } catch (Exception e) {
             super.marshal(value, writer, context);
+        } finally {
+            Context.exit();
         }
+    }
+
+    private Script getCompiledScript(Context context) {
+        Script script = compiledConversionScript.get();
+        if (script == null) {
+            script = context.compileString(conversionScript, "JSON stringifier", 1, null);
+            compiledConversionScript.compareAndSet(null, script);
+            script = compiledConversionScript.get();
+        }
+        return script;
     }
 }
